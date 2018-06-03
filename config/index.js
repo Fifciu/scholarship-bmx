e4fdd4810e643b8d07b07cee37e0a782b2d8b0ae
FIlip Jędrasik <filip.jdrasik@gmail.com>
2018-06-03 17:46:30 +0200

    Page

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/bmx/',
+    assetsPublicPath: '/scholarship-bmx/',
 
     /**
      * Source Maps
